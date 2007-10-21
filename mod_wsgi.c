a633f07768ec4a5409181f4b8469f95ea2d45d75
Graham.Dumpleton <devnull@localhost>
2007-10-21 01:52:08 +0000

    The ap_find_path_info() function behaves differently on Apache 1.3/2.0 than Apache 2.2, but require the Apache 2.2 behaviour. Thus duplicate this function from Apache 2.2 and use that copy for all versions of Apache. Doing so fixes problem whereby wrong application group (interpreter) can be selected when relying on default calculation for application group and repeating slashes appear in URL after the mount point of the application.

--- mod_wsgi.c
+++ mod_wsgi.c
@@ -604,6 +604,27 @@ typedef struct {
     int group_authoritative;
 } WSGIRequestConfig;
 
+static int wsgi_find_path_info(const char *uri, const char *path_info)
+{
+    int lu = strlen(uri);
+    int lp = strlen(path_info);
+
+    while (lu-- && lp-- && uri[lu] == path_info[lp]) {
+        if (path_info[lp] == '/') {
+            while (lu && uri[lu-1] == '/') lu--;
+        }
+    }
+
+    if (lu == -1) {
+        lu = 0;
+    }
+
+    while (uri[lu] != '\0' && uri[lu] != '/') {
+        lu++;
+    }
+    return lu;
+}
+
 static const char *wsgi_script_name(request_rec *r)
 {
     char *script_name = NULL;
@@ -613,7 +634,7 @@ static const char *wsgi_script_name(request_rec *r)
         script_name = apr_pstrdup(r->pool, r->uri);
     }
     else {
-        path_info_start = ap_find_path_info(r->uri, r->path_info);
+        path_info_start = wsgi_find_path_info(r->uri, r->path_info);
 
         script_name = apr_pstrndup(r->pool, r->uri, path_info_start);
     }
