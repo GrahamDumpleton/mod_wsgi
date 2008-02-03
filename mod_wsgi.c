7df5148faed652f5dd8efcb49daecb26eabcc36c
Graham.Dumpleton <devnull@localhost>
2008-02-03 10:06:23 +0000

    When wsgi.input.read() called with no argument all request input now returned again. This restores original functionality.

--- mod_wsgi.c
+++ mod_wsgi.c
@@ -1488,7 +1488,6 @@ static PyObject *Input_close(InputObject *self, PyObject *args)
 static PyObject *Input_read(InputObject *self, PyObject *args)
 {
     long size = -1;
-    int blocking = 1;
 
     PyObject *result = NULL;
     char *buffer = NULL;
@@ -1548,73 +1547,179 @@ static PyObject *Input_read(InputObject *self, PyObject *args)
     }
 
     /*
-     * If size is not specified for the number of bytes to read
-     * in, default to reading in standard Apache block size.
-     * Denote that blocking until we accumulate data of
-     * specified size is disabled in this case.
+     * First deal with case where size has been specified. After
+     * that deal with case where expected that all remaining
+     * data is to be read in and returned as one string.
      */
 
+    if (size > 0) {
+        /* Allocate string of the exact size required. */
 
-    if (size < 0) {
-        size = HUGE_STRING_LEN;
-        blocking = 0;
+        result = PyString_FromStringAndSize(NULL, size);
+
+        if (!result)
+            return NULL;
+
+        buffer = PyString_AS_STRING((PyStringObject *)result);
+
+        /* Copy any residual data from use of readline(). */
+
+        if (self->buffer && self->length) {
+            if (size >= self->length) {
+                length = self->length;
+                memcpy(buffer, self->buffer + self->offset, length);
+                self->offset = 0;
+                self->length = 0;
+            }
+            else {
+                length = size;
+                memcpy(buffer, self->buffer + self->offset, length);
+                self->offset += length;
+                self->length -= length;
+            }
+        }
+
+        /* If all data residual buffer consumed then free it. */
+
+        if (!self->length) {
+            free(self->buffer);
+            self->buffer = NULL;
+        }
+
+        /* Read in remaining data required to achieve size. */
+
+        if (length < size) {
+            while (length != size) {
+                Py_BEGIN_ALLOW_THREADS
+                n = ap_get_client_block(self->r, buffer + length,
+                                        size - length);
+                Py_END_ALLOW_THREADS
+
+                if (n == -1) {
+                    PyErr_SetString(PyExc_IOError, "request data read error");
+                    Py_DECREF(result);
+                    return NULL;
+                }
+                else if (n == 0) {
+                    /* Have exhausted all the available input data. */
+
+                    self->done = 1;
+                    break;
+                }
+
+                length += n;
+            }
+
+            /*
+             * Resize the final string. If the size reduction is
+             * by more than 25% of the string size, then Python
+             * will allocate a new block of memory and copy the
+             * data into it.
+             */
+
+            if (length != size) {
+                if (_PyString_Resize(&result, length))
+                    return NULL;
+            }
+        }
     }
+    else {
+        /*
+         * Here we are going to try and read in all the
+         * remaining data. First we have to allocate a suitably
+         * large string, but we can't fully trust the amount
+         * that the request structure says is remaining based on
+         * the original content length though, as an input
+         * filter can insert/remove data from the input stream
+         * thereby invalidating the original content length.
+         * What we do is allow for an extra 25% above what we
+         * have already buffered and what the request structure
+         * says is remaining. A value of 25% has been chosen so
+         * as to match best how Python handles resizing of
+         * strings. Note that even though we do this and allow
+         * all available content, strictly speaking the WSGI
+         * specification says we should only read up until content
+         * length. This though is because the WSGI specification
+         * is deficient in dealing with the concept of mutating
+         * input filters. Since read() with no argument is also
+         * not allowed by WSGI specification implement it in the
+         * way which is most logical and ensure that input data
+         * is not truncated.
+         */
 
-    /* Allocate string of the exact size required. */
+        size = self->length;
 
-    result = PyString_FromStringAndSize(NULL, size);
+        if (self->r->remaining > 0)
+            size += self->r->remaining;
 
-    if (!result)
-        return NULL;
+        size = size + (size >> 2);
 
-    buffer = PyString_AS_STRING((PyStringObject *)result);
+        if (size < 256)
+            size = 256;
 
-    /* Copy any residual data from use of readline(). */
+        /* Allocate string of the estimated size. */
 
-    if (self->buffer && self->length) {
-        if (size >= self->length) {
+        result = PyString_FromStringAndSize(NULL, size);
+
+        if (!result)
+            return NULL;
+
+        buffer = PyString_AS_STRING((PyStringObject *)result);
+
+        /*
+         * Copy any residual data from use of readline(). The
+         * residual should always be less in size than the
+         * string we have allocated to hold it, so can consume
+         * all of it.
+         */
+
+        if (self->buffer && self->length) {
             length = self->length;
             memcpy(buffer, self->buffer + self->offset, length);
             self->offset = 0;
             self->length = 0;
+
+            free(self->buffer);
+            self->buffer = NULL;
         }
-        else {
-            length = size;
-            memcpy(buffer, self->buffer + self->offset, length);
-            self->offset += length;
-            self->length -= length;
+
+        /* Now make first attempt at reading remaining data. */
+
+        Py_BEGIN_ALLOW_THREADS
+        n = ap_get_client_block(self->r, buffer + length, size - length);
+        Py_END_ALLOW_THREADS
+
+        if (n == -1) {
+            PyErr_SetString(PyExc_IOError, "request data read error");
+            Py_DECREF(result);
+            return NULL;
         }
-    }
+        else if (n == 0) {
+            /* Have exhausted all the available input data. */
 
-    /* If all data residual buffer consumed then free it. */
+            self->done = 1;
+        }
 
-    if (!self->length) {
-        free(self->buffer);
-        self->buffer = NULL;
-    }
+        length += n;
 
-    /*
-     * If not required to block and we already have some data
-     * from residual buffer we can return immediately.
-     */
+        /*
+         * Don't just assume that all data has been read if
+         * amount read was less than that requested. Still must
+         * perform a read which returns that no more data found.
+         */
 
-    if (!blocking && length != 0) {
-        if (length != size) {
-            if (_PyString_Resize(&result, length))
+        while (!self->done) {
+            /* Increase the size of the string by 25%. */
+
+            size = size + (size >> 2);
+
+            if (_PyString_Resize(&result, size))
                 return NULL;
-        }
 
-        return result;
-    }
+            buffer = PyString_AS_STRING((PyStringObject *)result);
 
-    /*
-     * Read in remaining data required to achieve size. If
-     * requested size of data wasn't able to be read in just
-     * return what was able to be read if blocking not required.
-     */
+            /* Now make succesive attempt at reading data. */
 
-    if (length < size) {
-        while (length != size) {
             Py_BEGIN_ALLOW_THREADS
             n = ap_get_client_block(self->r, buffer + length, size - length);
             Py_END_ALLOW_THREADS
@@ -1628,22 +1733,16 @@ static PyObject *Input_read(InputObject *self, PyObject *args)
                 /* Have exhausted all the available input data. */
 
                 self->done = 1;
-                break;
             }
 
             length += n;
-
-            /* Don't read more if not required to block. */
-
-            if (!blocking)
-                break;
         }
 
         /*
-         * Resize the final string. If the size reduction is
-         * by more than 25% of the string size, then Python
-         * will allocate a new block of memory and copy the
-         * data into it.
+         * Resize the final string. If the size reduction is by
+         * more than 25% of the string size, then Python will
+         * allocate a new block of memory and copy the data into
+         * it.
          */
 
         if (length != size) {
