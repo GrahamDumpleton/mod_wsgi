2048c21c793957e319f2196e5c20924af69cc75d
Graham.Dumpleton <devnull@localhost>
2009-01-27 13:07:29 +0000

    No longer populate error-notes in Apache request notes table with details of why mod_wsgi script failed. This has been done as information can be revealed in multi language error pages and doing so may be viewed as a security issue if any details pertaining to layout or machine or user/groups present is revealed.

--- mod_wsgi.c
+++ mod_wsgi.c
@@ -6954,7 +6954,6 @@ static void wsgi_log_script_error(request_rec *r, const char *e, const char *n)
         n = r->filename;
 
     message = apr_psprintf(r->pool, "%s: %s", e, n);
-    apr_table_set(r->notes, "error-notes", message);
 
     ap_log_rerror(APLOG_MARK, WSGI_LOG_ERR(0), r, "%s", message);
 }
