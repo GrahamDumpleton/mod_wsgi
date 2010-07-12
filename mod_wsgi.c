fdef274a5f623af536bcffbd9028cffa80b65ab0
Graham.Dumpleton <devnull@localhost>
2010-07-12 01:50:05 +0000

    Eliminate KeyError exception message on process shutdown when using Python 2.6.5+ and Python 3.1.2+. See issue #197.

--- mod_wsgi.c
+++ mod_wsgi.c
@@ -5660,6 +5660,26 @@ static apr_status_t wsgi_python_term()
     module = PyImport_ImportModule("atexit");
     Py_XDECREF(module);
 
+    /*
+     * In Python 2.6.5 and Python 3.1.2 the shutdown of
+     * threading was moved back into Py_Finalize() for the main
+     * Python interpreter. Because we shutting down threading
+     * ourselves, the second call results in errors being logged
+     * when Py_Finalize() is called and the shutdown function
+     * called a second time. The errors don't indicate any real
+     * problem and the threading module ignores them anyway.
+     * Whether we are using Python with this changed behaviour
+     * can only be checked by looking at run time version.
+     * Rather than try and add a dynamic check, create a fake
+     * 'dummy_threading' module as the presence of that shuts up
+     * the messages. It doesn't matter that the rest of the
+     * shutdown function still runs as everything is already
+     * stopped so doesn't do anything.
+     */
+
+    if (!PyImport_AddModule("dummy_threading"))
+        PyErr_Clear();
+
     Py_Finalize();
 
     wsgi_python_initialized = 0;
