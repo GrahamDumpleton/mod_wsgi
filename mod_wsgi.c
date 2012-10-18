8f73e29cc1a69cbb7dfd93e5c2e4baa42ddbe7bf
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-10-18 22:30:34 +1100

    Scoreboard handle in daemon mode should be set to NULL for Apache 2.4 to avoid crash in lingering close.

--- mod_wsgi.c
+++ mod_wsgi.c
@@ -10600,7 +10600,13 @@ static void wsgi_process_socket(apr_pool_t *p, apr_socket_t *sock,
      * will add their own input/output filters to the chain.
      */
 
+#if AP_MODULE_MAGIC_AT_LEAST(20110619,0)
+    /* For 2.4 a NULL sbh pointer should work. */
+    sbh = NULL;
+#else
+    /* For 2.2 a dummy sbh pointer is needed. */
     ap_create_sb_handle(&sbh, p, -1, 0);
+#endif
 
     c = (conn_rec *)apr_pcalloc(p, sizeof(conn_rec));
 
