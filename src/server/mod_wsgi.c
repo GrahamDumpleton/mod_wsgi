03f70967ecad8b3a59cb9ed3f09f83e44543a255
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-05-01 14:57:48 +1000

    Only initialise shutdown interpreter type object for Python 3.4.

--- src/server/mod_wsgi.c
+++ src/server/mod_wsgi.c
@@ -3667,9 +3667,12 @@ static void wsgi_python_child_init(apr_pool_t *p)
     PyType_Ready(&Adapter_Type);
     PyType_Ready(&Restricted_Type);
     PyType_Ready(&Interpreter_Type);
-    PyType_Ready(&ShutdownInterpreter_Type);
     PyType_Ready(&Dispatch_Type);
     PyType_Ready(&Auth_Type);
+
+#if PY_MAJOR_VERSION > 3 || (PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 4)
+    PyType_Ready(&ShutdownInterpreter_Type);
+#endif
 
     /* Initialise Python interpreter instance table and lock. */
 
