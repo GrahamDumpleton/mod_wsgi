65884a68c91603057e665e536dc9dfc4ae507e0b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-05-01 13:13:54 +1000

    Accidentally remove log level when dropping macro used for Apache 1.3.

--- src/server/mod_wsgi.c
+++ src/server/mod_wsgi.c
@@ -7110,7 +7110,7 @@ static void wsgi_setup_access(WSGIDaemonProcess *daemon)
     /* Setup the daemon process real and effective user. */
 
     if (setuid(daemon->group->uid) == -1) {
-        ap_log_error(APLOG_MARK, errno, wsgi_server,
+        ap_log_error(APLOG_MARK, APLOG_ALERT, errno, wsgi_server,
                      "mod_wsgi (pid=%d): Unable to change to uid=%ld.",
                      getpid(), (long)daemon->group->uid);
     }
