89274574da0e416c835721cfbd3c008bfec676dc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-08-02 11:26:53 +1000

    Increment version to 4.2.7.

--- src/server/wsgi_version.h
+++ src/server/wsgi_version.h
@@ -25,8 +25,8 @@
 
 #define MOD_WSGI_MAJORVERSION_NUMBER 4
 #define MOD_WSGI_MINORVERSION_NUMBER 2
-#define MOD_WSGI_MICROVERSION_NUMBER 6
-#define MOD_WSGI_VERSION_STRING "4.2.6"
+#define MOD_WSGI_MICROVERSION_NUMBER 7
+#define MOD_WSGI_VERSION_STRING "4.2.7"
 
 /* ------------------------------------------------------------------------- */
 
