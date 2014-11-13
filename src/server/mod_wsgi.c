ca149b09e8ccf268faa33c23ad809c5b4199a1ef
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-11-13 21:33:15 +1100

    Remove redundant variable serving no purpose.

--- src/server/mod_wsgi.c
+++ src/server/mod_wsgi.c
@@ -2764,11 +2764,8 @@ static int Adapter_run(AdapterObject *self, PyObject *object)
 static PyObject *Adapter_write(AdapterObject *self, PyObject *args)
 {
     PyObject *item = NULL;
-    PyObject *latin_item = NULL;
     const char *data = NULL;
     long length = 0;
-
-    /* XXX The use of latin_item here looks very broken. */
 
     if (!self->r) {
         PyErr_SetString(PyExc_RuntimeError, "request object has expired");
@@ -2781,7 +2778,6 @@ static PyObject *Adapter_write(AdapterObject *self, PyObject *args)
     if (!PyString_Check(item)) {
         PyErr_Format(PyExc_TypeError, "byte string value expected, value "
                      "of type %.200s found", item->ob_type->tp_name);
-        Py_XDECREF(latin_item);
         return NULL;
     }
 
@@ -2789,11 +2785,8 @@ static PyObject *Adapter_write(AdapterObject *self, PyObject *args)
     length = PyString_Size(item);
 
     if (!Adapter_output(self, data, length, item, 1)) {
-        Py_XDECREF(latin_item);
         return NULL;
     }
-
-    Py_XDECREF(latin_item);
 
     Py_INCREF(Py_None);
     return Py_None;
