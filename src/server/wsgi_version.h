2ec84a1edfbe5735112606bc58e3b34b872c2e93
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-12-20 17:08:28 +1100

    Increment version to 4.7.0.

--- src/server/wsgi_version.h
+++ src/server/wsgi_version.h
@@ -24,9 +24,9 @@
 /* Module version information. */
 
 #define MOD_WSGI_MAJORVERSION_NUMBER 4
-#define MOD_WSGI_MINORVERSION_NUMBER 6
-#define MOD_WSGI_MICROVERSION_NUMBER 9
-#define MOD_WSGI_VERSION_STRING "4.6.9"
+#define MOD_WSGI_MINORVERSION_NUMBER 7
+#define MOD_WSGI_MICROVERSION_NUMBER 0
+#define MOD_WSGI_VERSION_STRING "4.7.0"
 
 /* ------------------------------------------------------------------------- */
 
