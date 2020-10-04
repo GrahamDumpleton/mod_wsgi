4a6c2a78b9fedf820e367d03e8e01c767b787ce3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2020-10-04 16:52:10 +1100

    Increment mod_wsgi version to 4.8.0.

---
+++ docs/release-notes/version-4.8.0.rst
@@ -0,0 +1,7 @@
+=============
+Version 4.8.0
+=============
+
+Version 4.8.0 of mod_wsgi can be obtained from:
+
+  https://codeload.github.com/GrahamDumpleton/mod_wsgi/tar.gz/4.8.0
--- src/server/wsgi_version.h
+++ src/server/wsgi_version.h
@@ -24,9 +24,9 @@
 /* Module version information. */
 
 #define MOD_WSGI_MAJORVERSION_NUMBER 4
-#define MOD_WSGI_MINORVERSION_NUMBER 7
-#define MOD_WSGI_MICROVERSION_NUMBER 1
-#define MOD_WSGI_VERSION_STRING "4.7.1"
+#define MOD_WSGI_MINORVERSION_NUMBER 8
+#define MOD_WSGI_MICROVERSION_NUMBER 0
+#define MOD_WSGI_VERSION_STRING "4.8.0"
 
 /* ------------------------------------------------------------------------- */
 
