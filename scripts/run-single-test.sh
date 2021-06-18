374f6fb7873e630977ff973ab64c543fcbba39bf
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2021-06-18 12:21:36 +1000

    Extract test to separate script file.

---
+++ scripts/run-single-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+END=$((SECONDS+15))
+
+mod_wsgi-express setup-server tests/environ.wsgi --server-root httpd
+
+trap "httpd/apachectl stop" EXIT
+
+touch httpd/error.log
+
+httpd/apachectl start
+
+while [ ! -f httpd/httpd.pid ]; do
+    if [ $SECONDS -gt $END ]; then
+        echo 'Failed'
+        exit 1
+    fi
+
+    echo 'Waiting...'
+    sleep 1
+done
+
+sleep 2
+
+curl --verbose --fail --show-error http://localhost:8000
