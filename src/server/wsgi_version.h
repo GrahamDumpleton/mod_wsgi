f09c8a11d762cd107b1792e7e980b4a940755424
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2024-08-08 20:45:54 +1000

    Align version naming to latest Python standards.

--- src/server/wsgi_version.h
+++ src/server/wsgi_version.h
@@ -26,7 +26,7 @@
 #define MOD_WSGI_MAJORVERSION_NUMBER 5
 #define MOD_WSGI_MINORVERSION_NUMBER 0
 #define MOD_WSGI_MICROVERSION_NUMBER 1
-#define MOD_WSGI_VERSION_STRING "5.0.1dev1"
+#define MOD_WSGI_VERSION_STRING "5.0.1.dev1"
 
 /* ------------------------------------------------------------------------- */
 
