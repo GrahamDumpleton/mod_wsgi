ee8c5f4336ea6110ccaf58196bb55e3524e19246
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-04-13 12:59:18 +1000

    Use tp_getattro instead of deprecated tp_getattr in Restricted type.

--- src/server/wsgi_restrict.c
+++ src/server/wsgi_restrict.c
@@ -42,9 +42,10 @@ static void Restricted_dealloc(RestrictedObject *self)
     PyObject_Del(self);
 }
 
-static PyObject *Restricted_getattr(RestrictedObject *self, char *name)
+static PyObject *Restricted_getattr(RestrictedObject *self,
+                                    PyObject *Py_UNUSED(name))
 {
-    PyErr_Format(PyExc_IOError, "%s access restricted by mod_wsgi", self->s);
+    PyErr_Format(PyExc_OSError, "%s access restricted by mod_wsgi", self->s);
 
     return NULL;
 }
@@ -54,42 +55,42 @@ PyTypeObject Restricted_Type = {
     sizeof(RestrictedObject),                             /*tp_basicsize*/
     0,                                                    /*tp_itemsize*/
     /* methods */
-    (destructor)Restricted_dealloc,  /*tp_dealloc*/
-    0,                               /*tp_print*/
-    (getattrfunc)Restricted_getattr, /*tp_getattr*/
-    0,                               /*tp_setattr*/
-    0,                               /*tp_compare*/
-    0,                               /*tp_repr*/
-    0,                               /*tp_as_number*/
-    0,                               /*tp_as_sequence*/
-    0,                               /*tp_as_mapping*/
-    0,                               /*tp_hash*/
-    0,                               /*tp_call*/
-    0,                               /*tp_str*/
-    0,                               /*tp_getattro*/
-    0,                               /*tp_setattro*/
-    0,                               /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT,              /*tp_flags*/
-    0,                               /*tp_doc*/
-    0,                               /*tp_traverse*/
-    0,                               /*tp_clear*/
-    0,                               /*tp_richcompare*/
-    0,                               /*tp_weaklistoffset*/
-    0,                               /*tp_iter*/
-    0,                               /*tp_iternext*/
-    0,                               /*tp_methods*/
-    0,                               /*tp_members*/
-    0,                               /*tp_getset*/
-    0,                               /*tp_base*/
-    0,                               /*tp_dict*/
-    0,                               /*tp_descr_get*/
-    0,                               /*tp_descr_set*/
-    0,                               /*tp_dictoffset*/
-    0,                               /*tp_init*/
-    0,                               /*tp_alloc*/
-    0,                               /*tp_new*/
-    0,                               /*tp_free*/
-    0,                               /*tp_is_gc*/
+    (destructor)Restricted_dealloc,   /*tp_dealloc*/
+    0,                                /*tp_print*/
+    0,                                /*tp_getattr*/
+    0,                                /*tp_setattr*/
+    0,                                /*tp_compare*/
+    0,                                /*tp_repr*/
+    0,                                /*tp_as_number*/
+    0,                                /*tp_as_sequence*/
+    0,                                /*tp_as_mapping*/
+    0,                                /*tp_hash*/
+    0,                                /*tp_call*/
+    0,                                /*tp_str*/
+    (getattrofunc)Restricted_getattr, /*tp_getattro*/
+    0,                                /*tp_setattro*/
+    0,                                /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,               /*tp_flags*/
+    0,                                /*tp_doc*/
+    0,                                /*tp_traverse*/
+    0,                                /*tp_clear*/
+    0,                                /*tp_richcompare*/
+    0,                                /*tp_weaklistoffset*/
+    0,                                /*tp_iter*/
+    0,                                /*tp_iternext*/
+    0,                                /*tp_methods*/
+    0,                                /*tp_members*/
+    0,                                /*tp_getset*/
+    0,                                /*tp_base*/
+    0,                                /*tp_dict*/
+    0,                                /*tp_descr_get*/
+    0,                                /*tp_descr_set*/
+    0,                                /*tp_dictoffset*/
+    0,                                /*tp_init*/
+    0,                                /*tp_alloc*/
+    0,                                /*tp_new*/
+    0,                                /*tp_free*/
+    0,                                /*tp_is_gc*/
 };
 
 /* ------------------------------------------------------------------------- */
