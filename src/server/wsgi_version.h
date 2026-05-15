4b39916914119b6a02b6c083b7fe04c6e953abc7
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2026-05-15 17:39:45 +1000

    Increment to mod_wsgi 6.0.0.dev2 to test publishing to PyPi.

--- src/server/wsgi_version.h
+++ src/server/wsgi_version.h
@@ -26,7 +26,7 @@
 #define MOD_WSGI_MAJORVERSION_NUMBER 6
 #define MOD_WSGI_MINORVERSION_NUMBER 0
 #define MOD_WSGI_MICROVERSION_NUMBER 0
-#define MOD_WSGI_VERSION_STRING "6.0.0.dev1"
+#define MOD_WSGI_VERSION_STRING "6.0.0.dev2"
 
 /* ------------------------------------------------------------------------- */
 
